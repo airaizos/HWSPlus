37a839b9be897bc2e838a4de54c7b42826a50ba6
adrian <adrian@iraizos.me>
2022-12-22 10:48:17 +0100

    Initial Commit

---
+++ HWSPlus/HWSPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSPlusApp.swift
+//  HWSPlus
+//
+//  Created by Adrian Iraizos Mendoza on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HWSPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
