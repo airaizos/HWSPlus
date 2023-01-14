35c7fb6aaee185dd3b83759d41f8a1e294525574
adrian <adrian@iraizos.me>
2023-01-14 12:13:37 +0100

    pls logo bezier 2

--- HWSPlus/SwiftUIComponents/ShapeViewToBezierPaths/UIBezierPath_Extension.swift
+++ HWSPlus/SwiftUIComponents/ShapeViewToBezierPaths/UIBezierPath_Extension.swift
@@ -27,32 +27,39 @@ extension UIBezierPath {
         
         let path = UIBezierPath()
         
+        //ala baja
         path.move(to: CGPoint(x:0.51260025, y:0.503711875))
         path.addCurve(to: CGPoint(x: 0.307219375 , y:0.1818565), controlPoint1: CGPoint(x: 0.2788790416,y:0.478801583), controlPoint2: CGPoint(x: 0.4996933 , y:0.3418903))
-        path.addCurve(to: CGPoint(x: 0.3449774583 , y:0.2419564166), controlPoint1: CGPoint(x: 0.3202102916 , y:0.230494583), controlPoint2: CGPoint(x: 0.33336725 , y:0.237563583))
         
+        path.addCurve(to: CGPoint(x: 0.4 , y:0.239), controlPoint1: CGPoint(x: 0.4 , y:0.21), controlPoint2: CGPoint(x: 0.39999 , y:0.2304))
+        
+        //ala alta
         path.move(to: CGPoint(x:0.51260025 , y:0.503711875))
         path.addCurve(to: CGPoint(x: 0.3474351666 , y:0.138613916), controlPoint1: CGPoint(x: 0.3305725 , y:0.44388016), controlPoint2: CGPoint(x: 0.488838916 , y:0.31713016))
          
         path.addCurve(to: CGPoint(x: 0.58152475 , y:0.373500375),
                       controlPoint1: CGPoint(x: 0.5637255416 , y:0.239916),
-                      controlPoint2: CGPoint(x: 0.3667547083 , y:0.2814527916)
-                      )
+                      controlPoint2: CGPoint(x: 0.3667547083 , y:0.2814527916))
           
         
-        
+        // cabeza
         path.move(to: CGPoint(x:0.489581916 , y:0.270603))
-        path.addCurve(to: CGPoint(x: 0.66563916 , y:0.270603), controlPoint1: CGPoint(x: 0.541306375 , y:0.18612116), controlPoint2: CGPoint(x: 0.6157165416 , y:0.1840739583))
+        path.addCurve(to: CGPoint(x: 0.66563916 , y:0.275), controlPoint1: CGPoint(x: 0.541306375 , y:0.18612116), controlPoint2: CGPoint(x: 0.6157165416 , y:0.1840739583))
         
-        
+        // pico pecho
         path.move(to: CGPoint(x:0.6973816 , y:0.28267775))
         path.addCurve(to: CGPoint(x: 0.3595627916 , y:0.5560455416), controlPoint1: CGPoint(x: 0.52007983, y:0.2099337083), controlPoint2: CGPoint(x: 0.66563916, y:0.5560455416))
         
+        //cola
+        path.move(to: CGPoint(x:0.36 , y:0.5560454166))
         
-        path.move(to: CGPoint(x:0.359562916 , y:0.5560454166))
         path.addCurve(to: CGPoint(x: 0.40156283, y:0.5864182916), controlPoint1: CGPoint(x: 0.319055416, y:0.560268625), controlPoint2: CGPoint(x: 0.39074 , y:0.653519083))
-        path.addCurve(to: CGPoint(x: 0.429315083 , y:0.404776625), controlPoint1: CGPoint(x: 0.415439 , y:0.4389620416), controlPoint2: CGPoint(x: 0.408479625 , y:0.543864))
         
+        path.addCurve(to: CGPoint(x: 0.40 , y:0.45), controlPoint1: CGPoint(x: 0.41, y:0.42), controlPoint2: CGPoint(x: 0.41 , y:0.49))
+        
+        
+        
+       
         return path
     }
     
