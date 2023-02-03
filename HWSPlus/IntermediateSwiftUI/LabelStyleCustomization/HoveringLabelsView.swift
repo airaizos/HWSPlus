d37afc309a88ccbde068173fa6e5173d1c94caed
adrian <adrian@iraizos.me>
2023-02-03 17:10:35 +0100

    Hovering

---
+++ HWSPlus/IntermediateSwiftUI/LabelStyleCustomization/HoveringLabelsView.swift
@@ -0,0 +1,20 @@
+//
+//  HoveringLabelsView.swift
+//  HWSPlus
+//
+//  Created by Adrian Iraizos Mendoza on 3/2/23.
+//
+
+import SwiftUI
+
+struct HoveringLabelsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HoveringLabelsView_Previews: PreviewProvider {
+    static var previews: some View {
+        HoveringLabelsView()
+    }
+}
--- HWSPlus/IntermediateSwiftUI/LabelStyleCustomization/LabelStyleView.swift
+++ HWSPlus/IntermediateSwiftUI/LabelStyleCustomization/LabelStyleView.swift
@@ -9,43 +9,57 @@ import SwiftUI
 
 struct LabelStyleView: View {
     var body: some View {
-        VStack{
-            Label("Football", systemImage: "football.fill")
-            Label {
-                Text("Soccer")
-            } icon: {
-                Image(systemName: "soccerball")
+        NavigationView {
+            VStack{
+                Label("Football", systemImage: "football.fill")
+                Label {
+                    Text("Soccer")
+                } icon: {
+                    Image(systemName: "soccerball")
+                }
+                .labelStyle(IconOnlyLabelStyle())
+                
+                Label {
+                    Image(systemName: "tennisball")
+                        .foregroundColor(.green)
+                } icon: {
+                    Text("Tennis")
+                }
+                .labelStyle(TitleOnlyLabelStyle())
+                
+                Label {
+                    Text("volleyball")
+                } icon: {
+                    Image(systemName: "volleyball")
+                }
+                .labelStyle(VerticalLabelStyle())
+                
+                HoveringLabel(labelStyle: VerticalRevealingLabelStyle.self) {
+                    Text("Basketball")
+                } icon: {
+                    Image(systemName: "basketball")
+                }
+                HoveringLabel(labelStyle: HighlightingLabelStyle.self) {
+                    Text("Criket")
+                } icon: {
+                    Image(systemName: "cricket.ball")
+                }
+                
+                NavigationLink {
+                    HoveringLabelsView()
+                } label: {
+                    Image(systemName: "medal")
+                        .padding()              .background(Color.accentColor)
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
+                
             }
-            .labelStyle(IconOnlyLabelStyle())
+            .foregroundColor(Color.accentColor)
+            .font(.system(size: 50, weight: .bold))
             
-            Label {
-                Image(systemName: "tennisball")
-                    .foregroundColor(.green)
-            } icon: {
-                Text("Tennis")
-            }
-            .labelStyle(TitleOnlyLabelStyle())
-        
-            Label {
-                Text("volleyball")
-            } icon: {
-                Image(systemName: "volleyball")
-            }
-            .labelStyle(VerticalLabelStyle())
-            
-            HoveringLabel(labelStyle: VerticalRevealingLabelStyle.self) {
-                Text("Basketball")
-            } icon: {
-                Image(systemName: "basketball")
-            }
-            HoveringLabel(labelStyle: HighlightingLabelStyle.self) {
-                Text("Criket")
-            } icon: {
-                Image(systemName: "cricket.ball")
-            }
+            .navigationTitle("Label Style")
         }
-        .foregroundColor(Color.accentColor)
-        .font(.system(size: 50, weight: .bold))
     }
 }
 
